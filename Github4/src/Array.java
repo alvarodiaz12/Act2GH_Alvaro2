4f194579052679e7d2459ced1fe6ae9ade016f28
Alvaro <a-alvaro.diaz.casano@iespablopicasso.es>
2023-01-26 12:57:47 +0100

    Contador añadido

--- Github4/src/Array.java
+++ Github4/src/Array.java
@@ -20,5 +20,22 @@ public class Array {
     for (int i = 0; i < LONGITUDNUMEROS; i++) {
       System.out.print(numeros[i] + " ");
     }
+    System.out.println();
+    
+    int[] ar = {1, 2, 2, 1, 1, 3, 5,1, 2};
+    int contador=0;
+    int aux=ar[0];
+    for (int i = 0; i < ar.length; i++) {
+        if(aux == ar[i]){
+            contador++;
+        } else {
+            System.out.print(contador + ",");
+            contador=1;
+            aux=ar[i];
+        }
+    }
+    System.out.print(contador );
+    
+    System.out.println("Fin del programa");
   }  
 }
